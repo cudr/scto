daed38bc04745abdac0ceba3f0244b45e7c3feb3
cudr <codefaster@yandex.ru>
2019-07-09 16:00:10 +0300

    complete shiftCompare

--- src/compare/shiftCompare.test.ts
+++ src/compare/shiftCompare.test.ts
@@ -3,21 +3,41 @@ import { opGen } from "../operations";
 
 describe("shift compare", () => {
   it("take insert", () => {
-    expect(shiftCompare("foo", "foobar", 3)).toBe(
+    expect(shiftCompare("foo", "foobar", 3)).toStrictEqual(
       opGen("insert", 3, null, "bar")
     );
-    expect(shiftCompare("foo", "fbaroo", 1)).toBe(
+    expect(shiftCompare("foo", "fbaroo", 1)).toStrictEqual(
       opGen("insert", 1, null, "bar")
+    );
+    expect(shiftCompare("foo", "ffoo", 1)).toStrictEqual(
+      opGen("insert", 1, null, "f")
+    );
+    expect(shiftCompare("foo", "foofoo", 3)).toStrictEqual(
+      opGen("insert", 3, null, "foo")
+    );
+    expect(shiftCompare("foo", "bazfoo", 0)).toStrictEqual(
+      opGen("insert", 0, null, "baz")
     );
   });
 
   it("take drop", () => {
-    expect(shiftCompare("fbaroo", "foo", 1)).toBe(opGen("drop", 1, 3));
+    expect(shiftCompare("fbaroo", "foo", 1)).toStrictEqual(opGen("drop", 1, 3));
+    expect(shiftCompare("foofoo", "foo", 3)).toStrictEqual(opGen("drop", 3, 3));
+    expect(shiftCompare("foobazfoo", "foofoo", 3)).toStrictEqual(
+      opGen("drop", 3, 3)
+    );
+    expect(shiftCompare("bazfoo", "foo", 0)).toStrictEqual(opGen("drop", 0, 3));
   });
 
   it("take replace", () => {
-    expect(shiftCompare("fooxyzbar", "fooabcbar", 3)).toBe(
+    expect(shiftCompare("fooxyzbar", "fooabcbar", 3)).toStrictEqual(
       opGen("replace", 3, 3, "abc")
+    );
+    expect(shiftCompare("fooxyzbar", "fmtrdeger", 1)).toStrictEqual(
+      opGen("replace", 1, 7, "mtrdege")
+    );
+    expect(shiftCompare("dfoobar", "dxyzbar", 1)).toStrictEqual(
+      opGen("replace", 1, 3, "xyz")
     );
   });
 });
--- src/compare/shiftCompare.ts
+++ src/compare/shiftCompare.ts
@@ -1,28 +1,31 @@
 import { Insert, Drop, Replace } from "../operations";
 
-const shiftCompare = (origin, modifyed, i = 0, shift = 1) => {
-  if (i >= origin.length || i >= modifyed.length) return null;
+const shiftCompare = (origin, modifyed, i = 0, shift = 0) => {
+  if (i >= Math.max(origin.length, modifyed.length)) return null;
 
-  if (origin[i + shift] === modifyed[i + shift]) {
-    let op: Replace = {
-      type: "replace",
-      offset: i,
-      shift: shift - 1,
-      data: modifyed.slice(i, shift)
-    };
-    return op;
-  } else if (origin[i] === modifyed[i + shift]) {
+  let isShorter = !origin[i];
+  let isLonger = !modifyed[i];
+
+  if (isShorter || origin[i] === modifyed[i + shift]) {
     let op: Insert = {
       type: "insert",
       offset: i,
-      data: modifyed.slice(i, shift)
+      data: modifyed.slice(i, isShorter ? modifyed.length : i + shift)
     };
     return op;
-  } else if (origin[i + shift] === modifyed[i]) {
+  } else if (origin[i + shift] === modifyed[i + shift]) {
+    let op: Replace = {
+      type: "replace",
+      offset: i,
+      shift: shift,
+      data: modifyed.slice(i, i + shift)
+    };
+    return op;
+  } else if (isLonger || origin[i + shift] === modifyed[i]) {
     let op: Drop = {
       type: "drop",
       offset: i,
-      shift: shift
+      shift: isLonger ? origin.length - i : shift
     };
     return op;
   }
