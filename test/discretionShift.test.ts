a90141fb41c85c2c44ad58be551df96562c44c69
cudr <codefaster@yandex.ru>
2019-07-31 12:59:09 +0300

    repair stringsDiffToOps broken case

--- test/discretionShift.test.ts
+++ test/discretionShift.test.ts
@@ -1,28 +1,36 @@
 import discretionShift from "../src/compare/discretionShift";
 
+type Item = string[][] | string[];
+
+const pair = (pair: Item, match: any): [Item, any] => [pair, match];
+
+const listPairs = [
+  pair([[""], [""]], [0, 0]),
+  pair([["", "xyz"], ["bar"]], undefined),
+  pair([["", "bar"], [" "]], undefined),
+  pair([["bar"], ["bar"]], [0, 0]),
+  pair([["", "bar"], ["bar"]], [1, 0]),
+  pair([["", "", "bar"], [" ", "bar"]], [2, 1]),
+  pair([["xyz", "abc", "def", "xyz"], ["foo", "bar", "foo", "abc"]], [1, 3])
+];
+
+const stringPairs = [
+  pair(["ghifoo", "xyzbarfoo"], [3, 6]),
+  pair(["foo", "xyzbarfoo"], [0, 6]),
+  pair(["", " x"], [1, 0]),
+  pair(["   g", " x"], undefined)
+];
+
 describe("discretion shift", () => {
   it("find list shift pair", () => {
-    expect(discretionShift([""], [""])).toStrictEqual([0, 0]);
-    expect(discretionShift(["", "xyz"], ["bar"])).toBe(undefined);
-    expect(discretionShift(["", "bar"], [" "])).toBe(undefined);
-    expect(discretionShift(["bar"], ["bar"])).toStrictEqual([0, 0]);
-    expect(discretionShift(["", "bar"], ["bar"])).toStrictEqual([1, 0]);
-    expect(discretionShift(["", "", "bar"], [" ", "bar"])).toStrictEqual([
-      2,
-      1
-    ]);
-    expect(
-      discretionShift(
-        ["xyz", "abc", "def", "xyz"],
-        ["foo", "bar", "foo", "abc"]
-      )
-    ).toStrictEqual([1, 3]);
+    listPairs.forEach(([left, right]) =>
+      expect(discretionShift(left[0], left[1])).toStrictEqual(right)
+    );
   });
 
   it("find string shift pair", () => {
-    expect(discretionShift("ghifoo", "xyzbarfoo")).toStrictEqual([3, 6]);
-    expect(discretionShift("foo", "xyzbarfoo")).toStrictEqual([0, 6]);
-    expect(discretionShift("", " x")).toStrictEqual([1, 0]);
-    expect(discretionShift("   g", " x")).toBe(undefined);
+    stringPairs.forEach(([left, right]) =>
+      expect(discretionShift(left[0], left[1])).toStrictEqual(right)
+    );
   });
 });
--- test/text_source.ts
+++ test/text_source.ts
@@ -1,5 +1,5 @@
 export const origin =
-  "early Yet far hath!!! fought with great nature, That you with wisest apple - think on him, Together me remembrance      bananas of       ourselves! later";
+  "early Yet far hath!!! fought with great nature, That you with wisest apple - think on himTogether me remembrance      bananas of       ourselves! later";
 
 export const modifyed =
   "Yet so far hath discretion fought with nature That we with wisest sorrow — think on him, Together with remembrance of ourselves.";
